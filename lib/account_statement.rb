a59fe373cd387239690e4b882b30eb1d70cc6319
kehinde alaka <kennyalaka93@gmail.com>
2022-01-31 20:30:24 +0000

    created credit method for account statement that returns the amount a client deposits

--- lib/account_statement.rb
+++ lib/account_statement.rb
@@ -1,7 +1,11 @@
 require_relative 'account'
 class Account_Statement 
   attr_reader :account
-  def initialize(account)
+  def initialize(account = Account.new)
     @account = account
+  end
+
+  def credit
+    @account.credit
   end
 end
--- spec/account_statement_spec.rb
+++ spec/account_statement_spec.rb
@@ -3,8 +3,14 @@ require 'account_statement'
 describe Account_Statement do
   
   it 'initialize with an account' do
-    account = double(:account)
+    account = double(:double)
     statement = Account_Statement.new(account)
     expect(statement.account).not_to be_nil
+  end
+
+  it 'shows how much you have deposited' do
+    account = double :account, credit: 5
+    statement = Account_Statement.new(account)
+    expect(statement.credit).to eq(5)
   end
 end
